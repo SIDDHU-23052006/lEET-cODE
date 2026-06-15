bf59f254e878c0a06aa9097ad386211ce0e1f8f4
SIDDHU-23052006 <bsiddharth100@gmail.com>
2026-06-15 14:31:03 +0530

    String day 1

--- String/Practice.java
+++ String/Practice.java
@@ -2,31 +2,40 @@ import java.util.*;
 
 public class Practice {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // String s = " 8";
-        // int n = 4;
-        // System.out.println(s.charAt(0) == ' ');
-        int n = 1;
-        String s = "is2 sentence4 This1 a3";
-        String res = new String();
-        boolean flag = false;
-        boolean f = true;
-        while(f) {
-            String temp = new String();
-            for(int i = 0;i<s.length();i++) {
-                if((s.charAt(i)+"").equals(n+"")) {
-                    for(int j = i;j>=0 &&s.charAt(j)!=' ';j--) {
-                        temp = s.substring(j,i);
-                        flag = true;
-                    }
-                }
-                if(flag) {
-                    flag = false;
-                    res = res+temp+" ";
-                    n++;
-                }
-            }
-        }
-        System.out.println(res);
+        // Scanner sc = new Scanner(System.in);
+        // // String s = " 8";
+        // // int n = 4;
+        // // System.out.println(s.charAt(0) == ' ');
+        // int n = 1;
+        // String s = "is2 sentence4 This1 a3";
+        // String res = new String();
+        // boolean flag = false;
+        // boolean f = true;
+        // while(f) {
+        //     String temp = new String();
+        //     for(int i = 0;i<s.length();i++) {
+        //         if((s.charAt(i)+"").equals(n+"")) {
+        //             for(int j = i;j>=0 &&s.charAt(j)!=' ';j--) {
+        //                 temp = s.substring(j,i);
+        //                 flag = true;
+        //             }
+        //         }
+        //         if(flag) {
+        //             flag = false;
+        //             res = res+temp+" ";
+        //             n++;
+        //         }
+        //     }
+        // }
+        // System.out.println(res);
+        // int n = 97+25;
+        // System.out.println((char)n);
+        // int num = Integer.parseInt("11")+96;
+        // char c = (char)num;
+        // String r = new String();
+        // r = r+"1"+"2";
+        // System.out.println(r);
+        int n = (int) 'c';
+        System.out.println((int)'1');
     }
 }
